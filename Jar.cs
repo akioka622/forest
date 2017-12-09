57e4f6eb24789c1a4db93592960f479fdc7679d2
akioka622 <34388419+akioka622@users.noreply.github.com>
2017-12-09 14:48:34 +0900

    Add files via upload

---
+++ Jar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jar : MonoBehaviour
+{
+
+    private static string JAVA_CLASS_NAME = "com.exampletgl.kaede.mylibrary1126.BuildConfig";
+    private static string JAVA_METHOD_NAME = "BuildConfig";
+
+    private static string COROUTINES_NAME = "sleep";
+    private static int WAIT_SECOND = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(COROUTINES_NAME);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //「コルーチン」で呼び出すメソッド
+    IEnumerator sleep()
+    {
+        yield return new WaitForSeconds(WAIT_SECOND);
+        using (AndroidJavaClass plugin = new AndroidJavaClass(JAVA_CLASS_NAME))
+        {
+            plugin.CallStatic(JAVA_METHOD_NAME);
+        }
+    }
+}
